0c0d40ff75e231cdd27105f944f6f167038f818a
Denis G <denis.ga@cmstuning.com>
2023-09-18 00:02:16 +0300

    feat: implemented functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const chars = Array.from(str);
+
+  let dictionary = Object.assign({}, ...bracketsConfig.map((x) => ({[x[0]]: x[1]})));
+
+  chars.forEach(element => {
+    if(dictionary[element] !== undefined && stack[stack.length-1] !== element){
+      stack.push(dictionary[element]);
+    }
+    else{
+      if(stack[stack.length-1] === element){
+        stack.splice(-1);     
+      }
+      else{
+        stack.push(element);
+      }
+    }
+  });
+
+  if(stack.length === 0)
+    return true;
+  else
+    return false;
 }
